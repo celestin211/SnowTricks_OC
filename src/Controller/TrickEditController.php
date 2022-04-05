eaab5948ce38a0fcab3b75ad33be4edab15c3a92
BOSONGO <celestin.mombela@yahoo.fr>
2022-04-06 00:43:10 +0200

    Update TrickEditController.php

--- src/Controller/TrickEditController.php
+++ src/Controller/TrickEditController.php
@@ -16,63 +16,61 @@ use Symfony\Component\Routing\Annotation\Route;
 class TrickEditController extends AbstractController
 {
 
-
-      /**
-       * Edit trick
-       *
-       * @Route("/trick/{id}/edit", name="trick_edit")
-       * @Security(
-       *      "user === trick.getUser() || is_granted('ROLE_ADMIN')",
-       *      message = "Ce trick ne vous appartient pas !"
-       * )
-       *
-       * @param Request $request
-       * @param EntityManagerInterface $manager
-       * @param Trick $trick
-       * @param ImageRepository $repo_image
-       *
-       * @return Response
-       */
-      public function edit(Request $request, EntityManagerInterface $manager, Trick $trick, ImageRepository $repo_image): Response
-      {
-          $path = $this->getParameter('images_directory');
-          $images = $repo_image->findBy(array('trick' => $trick->getId()));
-          $form = $this->createForm(TrickType::class, $trick);
-          foreach ($trick->getImages() as $image) {
-              $image->setPath($path);
-          }
-          $form->handleRequest($request);
-          if ($form->isSubmitted() && $form->isValid()) {
-
-              $trick->setPath($path);
-
-              for ($i = 0; $i < 8; $i++) {
-                  foreach ($trick->getImages() as $image) {
-                      $image->setPath($path);
-                  }
-              }
-
-              for ($i = 0; $i < 8; $i++) {
-                  foreach ($trick->getVideos() as $video) {
-                      if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $video->getUrl(), $match)) {
-                          $video_id = $match[1];
-                          $video->setUrl('https://www.youtube.com/embed/' . $video_id);
-                      }
-                  }
-              }
-
-              $trick->setUpdatedAt(new \Datetime);
-              $manager->flush();
-              $this->addFlash('success', 'Bravo votre trick a été modifié avec succés !');
-              return $this->redirectToRoute('trick_show', ['id' => $trick->getId()]);
-          }
-
-          return $this->render('trick/edit-trick.html.twig', [
-              'form' => $form->createView(),
-              'trick' => $trick,
-              'images' => $images,
-          ]);
-
-      }
-
+/**
+ * Edit trick
+ *
+ * @Route("/trick/{slug}/edit", name="trick_edit")
+ * @Security(
+ *      "user === trick.getUser() || is_granted('ROLE_ADMIN')",
+ *      message = "Ce trick ne vous appartient pas !"
+ * )
+ *
+ * @param Request $request
+ * @param EntityManagerInterface $manager
+ * @param Trick $trick
+ * @param ImageRepository $repo_image
+ *
+ * @return Response
+ */
+public function edit(Request $request, EntityManagerInterface $manager, Trick $trick, ImageRepository $repo_image): Response
+{
+    $path = $this->getParameter('images_directory');
+    $images = $repo_image->findBy(array('trick' => $trick->getId()));
+    $form = $this->createForm(TrickType::class, $trick);
+    foreach ($trick->getImages() as $image) {
+        $image->setPath($path);
     }
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+
+        $trick->setPath($path);
+
+        for ($i = 0; $i < 8; $i++) {
+            foreach ($trick->getImages() as $image) {
+                $image->setPath($path);
+            }
+        }
+
+        for ($i = 0; $i < 8; $i++) {
+            foreach ($trick->getVideos() as $video) {
+                if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $video->getUrl(), $match)) {
+                    $video_id = $match[1];
+                    $video->setUrl('https://www.youtube.com/embed/' . $video_id);
+                }
+            }
+        }
+
+        $trick->setUpdatedAt(new \Datetime);
+        $manager->flush();
+        $this->addFlash('success', 'Votre trick a été modifié avec succés !');
+        return $this->redirectToRoute('trick_show', ['id' => $trick->getId(), 'slug'=>$trick->getSlug()]);
+    }
+
+    return $this->render('trick/edit-trick.html.twig', [
+        'form' => $form->createView(),
+        'trick' => $trick,
+        'images' => $images,
+    ]);
+  }
+}
+
